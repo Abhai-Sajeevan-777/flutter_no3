71773418923232c30a15e376f0f4518f1e25a5d0
Abhai-Sajeevan-777 <abhaisajeevan73@gmail.com>
2025-02-20 20:12:11 +0530

    ..

--- lib/page_view_samp.dart
+++ lib/page_view_samp.dart
@@ -5,7 +5,14 @@ class PageViewSamp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> images = [
+      "https://images.pexels.com/photos/30635909/pexels-photo-30635909/free-photo-of-rustic-forest-scene-with-weathered-tree-trunk.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "https://images.pexels.com/photos/30599359/pexels-photo-30599359/free-photo-of-bare-trees-and-blue-sky-with-wispy-clouds.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "https://images.pexels.com/photos/30599819/pexels-photo-30599819/free-photo-of-majestic-stone-viaduct-in-rocky-landscape.jpeg?auto=compress&cs=tinysrgb&w=600"
+    ];
+
     return Scaffold(
+      appBar: AppBar(),
       body: PageView.builder(
         // children: [
         //   Container(
@@ -24,8 +31,14 @@ class PageViewSamp extends StatelessWidget {
         //     color: Colors.pink,
         //   ),
         // ],
+        itemCount: images.length, scrollDirection: Axis.vertical,
         itemBuilder: (context, index) => Container(
-          color: Colors.yellow,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage(images[index]))),
+          margin: EdgeInsets.all(10),
+          //color: Colors.yellow,
+          alignment: Alignment.center,
+          child: Text("$index"),
         ),
       ),
     );
--- lib/reels_page.dart
+++ lib/reels_page.dart
@@ -5,8 +5,71 @@ class ReelsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> images = [
+      "https://images.pexels.com/photos/159515/football-american-football-runner-player-159515.jpeg?auto=compress&cs=tinysrgb&w=400",
+      "https://images.pexels.com/photos/1657328/pexels-photo-1657328.jpeg?auto=compress&cs=tinysrgb&w=400"
+    ];
     return Scaffold(
-      appBar: AppBar(),
-    );
+        backgroundColor: Colors.blue,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.transparent,
+          actions: [
+            Icon(
+              Icons.camera_alt_outlined,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 10,
+            )
+          ],
+        ),
+        body: PageView(children: [
+          Positioned(
+            bottom: 50,
+            right: 10,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.comment,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                      size: 30,
+                    )),
+              ],
+            ),
+          ),
+        ]));
   }
 }
